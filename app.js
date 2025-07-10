0d0f58d744a741b85eef8794b36e9a95e18e5cc1
Fabius Gasber <fabiusgasber@gmail.com>
2025-07-10 17:43:09 +0200

    style: add styling

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const path = require("node:path");
 const router = require("./routes/indexRouter.js");
 const app = express();
+const assetsPath = path.join(__dirname, "public");
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(assetsPath));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use("/", router);
