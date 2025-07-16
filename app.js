9b3bd7dfd9b8568b65502493447b86ac2d394ee3
Fabius Gasber <fabiusgasber@gmail.com>
2025-07-16 18:23:28 +0200

    fix: use .env port or 3000

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require("node:path");
 const router = require("./routes/indexRouter.js");
 const app = express();
 const assetsPath = path.join(__dirname, "public");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(assetsPath));
@@ -11,4 +11,4 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use("/", router);
 
-app.listen(PORT, () => console.log(`Server listens on localhost:${PORT}`))
+app.listen(PORT);
